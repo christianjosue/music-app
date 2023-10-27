a4c9977f97b603ce203ec4f09195d7607bb7b832
christianjosuefuentes <christianjosuefuentes@gmail.com>
2023-10-27 12:21:01 +0200

    Scaffolding and a bit of UI

---
+++ spotify-app/src/App.vue
@@ -0,0 +1,71 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container">
+    <div class="menu">
+        <div class="menu-item active">
+            <a href="#" class="active"><font-awesome-icon icon="fa-solid fa-house" class="icon" />Inicio</a>
+        </div>
+        <div class="menu-item">
+            <a href="#"><font-awesome-icon icon="fa-solid fa-magnifying-glass" class="icon" />Buscar</a>
+        </div>
+    </div>
+    <div class="main"></div>
+  </div>
+</template>
+
+<style scoped>
+    .container {
+        display: flex;
+        width: 100%;
+        height: 100%;
+        border-radius: 20px;
+    }
+
+    .menu {
+        width: 22%;
+        background-color: #111;
+        border-radius: 10px;
+        margin-right: 10px;
+        box-sizing: border-box;
+        padding: 25px;
+    }
+
+    .main {
+        width: 78%;
+        background-color: #111;
+        border-radius: 10px;
+    }
+
+    .menu-item {
+        font-size: 17px;
+        font-weight: 400;
+        box-sizing: border-box;
+        margin-bottom: 30px;
+        display: flex;
+        align-items: center;
+    }
+
+    .menu-item a {
+        display: flex;
+        align-items: center;
+        text-decoration: none;
+        color: rgb(190, 190, 190);
+    }
+
+    .menu-item a:hover {
+        color: white;
+        transition: all .3s ease;
+    }
+
+    .icon {
+        margin-right: 18px;
+        height: 22px;
+    }
+
+    .active {
+        color: white !important;
+    }
+</style>
---
+++ spotify-app/src/main.js
@@ -0,0 +1,22 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faHouse } from '@fortawesome/free-solid-svg-icons'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faUserSecret, faHouse, faMagnifyingGlass)
+
+createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount('#app')
