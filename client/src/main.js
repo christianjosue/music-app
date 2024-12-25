f73268597393d608a7587d0a007cb145f9ddd5d2
christian <christianjosuefuentes@gmail.com>
2024-12-25 17:18:03 +0100

    Improve the usage of fontawesome library

--- client/src/main.js
+++ client/src/main.js
@@ -11,40 +11,76 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 /* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-/* import specific icons */
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
-import { faHouse } from '@fortawesome/free-solid-svg-icons'
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
-import { faCirclePlay } from '@fortawesome/free-solid-svg-icons'
-import { faVolumeXmark } from '@fortawesome/free-solid-svg-icons'
-import { faVolumeOff } from '@fortawesome/free-solid-svg-icons'
-import { faVolumeLow } from '@fortawesome/free-solid-svg-icons'
-import { faVolumeHigh } from '@fortawesome/free-solid-svg-icons'
-import { faXmark } from '@fortawesome/free-solid-svg-icons'
-import { faHeadphones } from '@fortawesome/free-solid-svg-icons'
-import { faShuffle } from '@fortawesome/free-solid-svg-icons'
-import { faPlus } from '@fortawesome/free-solid-svg-icons'
-import { faCircleDown } from '@fortawesome/free-solid-svg-icons'
-import { faEllipsis } from '@fortawesome/free-solid-svg-icons'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
-import { faClock } from '@fortawesome/free-solid-svg-icons'
-import { faPlay } from '@fortawesome/free-solid-svg-icons'
-import { faPause } from '@fortawesome/free-solid-svg-icons'
-import { faMicrophone } from '@fortawesome/free-solid-svg-icons'
-import { faRepeat } from '@fortawesome/free-solid-svg-icons'
-import { faTrash } from '@fortawesome/free-solid-svg-icons';
-import { faPencil } from '@fortawesome/free-solid-svg-icons';
-import { faMusic } from '@fortawesome/free-solid-svg-icons';
-import { faCirclePlus } from '@fortawesome/free-solid-svg-icons';
-import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
-import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
-import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
+/* Import specific icons */
+import {
+    faUserSecret,
+    faHouse,
+    faMagnifyingGlass,
+    faCirclePlay,
+    faVolumeXmark,
+    faVolumeOff,
+    faVolumeLow,
+    faVolumeHigh,
+    faXmark,
+    faHeadphones,
+    faShuffle,
+    faPlus,
+    faCircleDown,
+    faEllipsis,
+    faBars,
+    faClock,
+    faPlay,
+    faPause,
+    faMicrophone,
+    faRepeat,
+    faTrash,
+    faPencil,
+    faMusic,
+    faCirclePlus,
+    faCircleXmark,
+    faArrowUp,
+    faArrowDown,
+    faArrowLeft,
+    faUser,
+} from '@fortawesome/free-solid-svg-icons';
+
 import * as Vibrant from 'node-vibrant';
 
-/* add icons to the library */
-library.add(faUserSecret, faHouse, faMagnifyingGlass, faCirclePlay, faVolumeXmark, faVolumeOff, faVolumeLow, faVolumeHigh, faXmark, faHeadphones, faShuffle, faPlus, faCircleDown, faEllipsis, faBars, faClock, faPlay, faPause, faMicrophone, faRepeat, faTrash, faPencil, faMusic, faCirclePlus, faCircleXmark, faArrowUp, faArrowDown, faArrowLeft, faUser)
+/* Group icons in an array */
+const icons = [
+    faUserSecret,
+    faHouse,
+    faMagnifyingGlass,
+    faCirclePlay,
+    faVolumeXmark,
+    faVolumeOff,
+    faVolumeLow,
+    faVolumeHigh,
+    faXmark,
+    faHeadphones,
+    faShuffle,
+    faPlus,
+    faCircleDown,
+    faEllipsis,
+    faBars,
+    faClock,
+    faPlay,
+    faPause,
+    faMicrophone,
+    faRepeat,
+    faTrash,
+    faPencil,
+    faMusic,
+    faCirclePlus,
+    faCircleXmark,
+    faArrowUp,
+    faArrowDown,
+    faArrowLeft,
+    faUser,
+];
+
+/* Add icons to the library */
+library.add(...icons);
 
 createApp(App)
 .component('font-awesome-icon', FontAwesomeIcon)
