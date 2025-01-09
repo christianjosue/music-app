0d9b0ec1e23fe6eb833e3b155b8b37bc8aa64241
christianjosuefuentes <christianjosuefuentes@gmail.com>
2025-01-09 02:10:58 +0100

    Fix hot reloading in Vue server on Windows by activating polling in vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -12,5 +12,11 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    watch: {
+      usePolling: true
+    },
+    host: '0.0.0.0'
   }
 })
